bd5ea4ecbcea1a234a5a2207fe0c577797be0c61
alexander34ro <ardeleanalexandru3@gmail.com>
2020-07-31 14:24:16 +0200

    Fix

--- 2-Distributed-Setup/2_json/to_json.js
+++ 2-Distributed-Setup/2_json/to_json.js
@@ -2,5 +2,9 @@ if (require.main === module) {
   const input = process.argv[2];
   const parsed = JSON.parse(input);
 
-  console.log(parsed);
+  const simplified = {}
+  simplified.level = parsed.level;
+  simplified.charging = parsed.charging;
+
+  console.log(simplified);
 }
