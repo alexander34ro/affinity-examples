faaa50fa0d09b1d6429e9f01ee99ff8fbcdc5469
alexander34ro <ardeleanalexandru3@gmail.com>
2020-07-31 10:46:41 +0200

    Refactoring

--- 1-Localized-Communication/4_save/save.js
+++ 1-Localized-Communication/4_save/save.js
@@ -5,7 +5,6 @@ if (require.main === module) {
   const input = process.argv[2];
   const img_path = '4_decoded_image.jpg';
 
-  console.log(input);
   const data = Base64String.decompressFromUTF16(input);
   console.log(data);
   const image = new Buffer(data, 'base64');
