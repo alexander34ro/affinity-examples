1cb688f976d4a26f6e892d8c6716853766f06681
alexander34ro <ardeleanalexandru3@gmail.com>
2020-09-06 08:20:40 +0200

    Update

--- 3-Distributed-Sensing/2_json/to_json.js
+++ 3-Distributed-Sensing/2_json/to_json.js
@@ -1,15 +1,10 @@
 if (require.main === module) {
   const input = process.argv[2];
   const parsed = JSON.parse(input);
-  console.log(parsed);
 
   const simplified = {}
-  simplified.level = parsed.level;
-  simplified.charging = parsed.charging;
-  if (simplified.level === undefined) {
-    simplified.level = parsed.percentage;
-    simplified.charging = parsed.plugged != "UNPLUGGED";
-  }
+  simplified.accelerometer = parsed["LSM6DSL Accelerometer"];
+  simplified.light = parsed["TMD3725 Light"];
 
   console.log(JSON.stringify(simplified));
 }
