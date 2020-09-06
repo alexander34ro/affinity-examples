361531cfe1fbf5d08354c80c3eb6603a8013fcbe
alexander34ro <ardeleanalexandru3@gmail.com>
2020-09-06 08:24:49 +0200

    Update

--- 3-Distributed-Sensing/3_emotion_analyzer/emotion_analyzer.py
+++ 3-Distributed-Sensing/3_emotion_analyzer/emotion_analyzer.py
@@ -9,6 +9,10 @@ def f():
     data = json.loads(sys.argv[1])
     accelerometer = data["accelerometer"]
     light = data["light"]
+
+    print(accelerometer)
+    print(light)
+    print(light[0])
     
     status = "CRITICAL"
     if light > 25:
