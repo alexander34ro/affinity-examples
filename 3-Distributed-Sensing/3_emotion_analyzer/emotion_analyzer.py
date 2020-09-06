cadd94d46fb55117972f63d35ef2796407092fae
alexander34ro <ardeleanalexandru3@gmail.com>
2020-09-06 08:29:11 +0200

    Update

--- 3-Distributed-Sensing/3_emotion_analyzer/emotion_analyzer.py
+++ 3-Distributed-Sensing/3_emotion_analyzer/emotion_analyzer.py
@@ -7,21 +7,21 @@ import time
 
 def f():
     data = json.loads(sys.argv[1])
-    accelerometer = data["accelerometer"]
-    light = data["light"]
-
-    print(accelerometer)
-    print(light)
-    print(light[0])
+    accelerometer_x = data["accelerometer"][0]
+    accelerometer_y = data["accelerometer"][1]
+    accelerometer_z = data["accelerometer"][2]
+    light = data["light"][0]
     
-    status = "CRITICAL"
-    if light > 25:
-      status = "LOW"
-    if light > 50:
-      status = "GOOD"
-    if accelerometer:
-      status = "CHARGING"
-    return status
+    emotion = "Sad"
+    if light > 15:
+      emotion = "Happy"
+    if light > 20:
+      emotion = "Excited"
+    if accelerometer_x > accelerometer_y:
+      emotion = "Angry"
+    if accelerometer_y > accelerometer_z:
+      emotion = "Scared"
+    return emotion
 
 
 if __name__ == '__main__':
